9e81e640930665f23da2c8ae624f5139a1d6ca9d
Valeriya Abdrakhmanova <valeriya.avt@gmail.com>
2020-10-07 17:20:00 +0200

    Version 2 with comments

--- sources/main.c
+++ sources/main.c
@@ -24,7 +24,7 @@ char *get_word(char *end) {
     return word;
 }
 
-char **get_list() {
+char **get_cmd() {
     char **list = NULL;
     char end;
     int i = 0;
@@ -59,38 +59,38 @@ void clear(char **list) {
 
 int search_symbol(char **list, int *input, int *output, int *i) {
     int n = *i;
-    int fd = -1;
-    char input_symbol[] = ">", output_symbol[] = "<";
-    int input_flag = *input, output_flag = *output;
-    while ((list[n] != NULL) && (!input_flag) && (!output_flag)) {
-        if (!strcmp(list[n], input_symbol) && list[n + 1] != NULL) {
+    int fd = -1; // изначально файл не открыт
+    char input_symbol[] = ">", output_symbol[] = "<"; // сохраним символы, которые будем искать
+    int input_flag = *input, output_flag = *output; // для удобства заведём локальные флаги
+    while ((list[n] != NULL) && (!input_flag) && (!output_flag)) { // будем бежать по строке, пока не найдём < или >
+        if (!strcmp(list[n], input_symbol) && list[n + 1] != NULL) { // если нашли знак >
             fd = open(list[n + 1], O_WRONLY | O_CREAT | O_TRUNC,
-                 S_IRUSR | S_IWUSR);
-            input_flag = 1;
+                 S_IRUSR | S_IWUSR); // открыли файл на запись (считаем, что имя файла идёт сразу после >)
+            input_flag = 1; // выставили флаг
         }
-        if (!strcmp(list[n], output_symbol) && list[n + 1] != NULL) {
-            fd = open(list[n + 1], O_RDONLY);
-            output_flag = 1;
+        if (!strcmp(list[n], output_symbol) && list[n + 1] != NULL) { // если нашли знак <
+            fd = open(list[n + 1], O_RDONLY); // октрыли файл на чтение
+            output_flag = 1; // выставили флаг
         }
-        n++;
+        n++; // обновляем счётчик
     }
-    *input = input_flag;
+    *input = input_flag; // передали состояние флагов
     *output = output_flag;
-    *i = n;
-    return fd;
+    *i = n; // в list[n] лежит название файла
+    return fd; // вернули файловый дескриптор
 }
 
 int checking_flags(char **list, int input_flag, int output_flag, int fd, int n) {
     char *tmp;
-    if (input_flag) {
-        dup2(fd, 1);
-        free(list[n]);
-        tmp = list[n - 1];
-        list[n - 1] = NULL;
-        free(tmp);
+    if (input_flag) { // если нашли знак >
+        dup2(fd, 1); // направили вывод программы в файл
+        free(list[n]); // удалили элемент с названием файла
+        tmp = list[n - 1]; //
+        list[n - 1] = NULL; // конец строки указывает на NULL
+        free(tmp); // удалили элемент со знаком >
     }
     if (output_flag) {
-        dup2(fd, 0);
+        dup2(fd, 0); // теперь считывать будем из файла
         free(list[n]);
         tmp = list[n - 1];
         list[n - 1] = NULL;
@@ -104,28 +104,91 @@ int checking_flags(char **list, int input_flag, int output_flag, int fd, int n) 
     return 0;
 }
 
+void create_pipe(char **cmd1, char **cmd2) {
+    int fd[2];
+    pipe(fd);
+    if (fork() == 0) {
+        dup2(fd[1], 1);
+        close(fd[0]);
+        close(fd[1]);
+        execvp(cmd1[0], cmd1);
+    }
+    if (fork() == 0) {
+        dup2(fd[0], 0);
+        close(fd[0]);
+        close(fd[1]);
+        execvp(cmd2[0], cmd2);
+    }
+    close(fd[0]);
+    close(fd[1]);
+    wait(NULL);
+    wait(NULL);
+}
+
+int search_separator(char **list) {
+    int n = 0;
+    char separator_symbol[] = "|";
+    while (list[n] != NULL) { // будем бежать по строке, пока не найдём |
+        if (!strcmp(list[n], separator_symbol) && list[n + 1] != NULL) { // если нашли |
+            char *cmd1[] = {list[0], NULL};
+            char *cmd2[] = {list[2], NULL};
+            create_pipe(cmd1, cmd2);
+            return 1;
+        }
+        n++;
+    }
+    return 0;
+}
+
+// char ***get_list(char **list) {
+//     int n = 0, i = 0;
+//     char **array = list;
+//     char ***cmd_io_array = NULL;
+//     char **tmp = NULL;
+//     char separator[] = "|";
+//     while (list[n] != NULL) {
+//         cmd_io_array = realloc(cmd_io_array, (i + 1) * sizeof(char **));
+//         while (strcmp(list[n]), separator)) {
+//             cmd_io_array[i] =
+//             n++;
+//         }
+//         if (!strcmp(list[n], separator)) {
+//
+//         }
+//         i++;
+//     }
+//     cmd_io_array[i] = NULL;
+//     return cmd_io_array;
+// }
+
 int main(int argc, char **argv) {
-    int n = 0, fd;
-    char **list = get_list();
+    int n = 0, fd; //
+    char **list = get_cmd();
+//    char ***cmd_io_array = get_list(list);
     char finish1[] = "exit", finish2[] = "quit";
-    int input_flag, output_flag;
-    while (strcmp(list[0], finish1) && strcmp(list[0], finish2)) {
+    int input_flag, output_flag, separator_flag;
+    while (strcmp(list[0], finish1) && strcmp(list[0], finish2)) { // делаем fork, пока не встретим exit или quit
     //    print_list(list, size);
-        input_flag = 0;
-        output_flag = 0;
-        if (fork() > 0) {
-            wait(NULL);
-        } else {
-            fd = search_symbol(list, &input_flag, &output_flag, &n);
-                if (checking_flags(list, input_flag, output_flag, fd, n))
-                    return 1;
+        n = 0; // ?
+        input_flag = 0; // еще не встретили знак >
+        output_flag = 0; // ещё не встретили знак <
+        separator_flag = 0; // ещё не встретили |
+        separator_flag = search_separator(list);
+        if (!separator_flag) {
+            if (fork() > 0) { // родительский процесс ждёт завершения дочернего
+                wait(NULL);
+            } else {
+                fd = search_symbol(list, &input_flag, &output_flag, &n); // в дочернем процессе ищем знаки < > в строке
+                    if (checking_flags(list, input_flag, output_flag, fd, n)) // проверяем, нашли ли мы < >
+                        return 1; //если попали сюда - произошла ошибка при вызове exec в  checking_flags
+            }
+            if (input_flag || output_flag) { // если какой-либо из флагов выставлен, закрываем файловый дескриптор
+                close(fd);
+            }
         }
         clear(list);
-        list = get_list();
-        if (input_flag || output_flag) {
-            close(fd);
-        }
+        list = get_cmd();
     }
-    clear(list);
+    clear(list); //удаляем строку с exit или quit
     return 0;
 }
