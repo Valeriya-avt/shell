b49c4d91db513b1cf0251c443e901f8a3025d44e
Valeriya Abdrakhmanova <valeriya.avt@gmail.com>
2020-10-08 19:57:58 +0200

    Version 2 of main

--- sources/main.c
+++ sources/main.c
@@ -92,31 +92,16 @@ void clear_cmd(char **cmd) {
     free(cmd);
 }
 
-char **search_io_symbol(char **list, int *input, int *output) {
-    int n = 0;
-    int fd = -1; // изначально файл не открыт
-    char input_symbol[] = ">", output_symbol[] = "<"; // сохраним символы, которые будем искать
-    char *tmp = NULL;
-    int input_flag = *input, output_flag = *output; // для удобства заведём локальные флаги
-    while ((list[n] != NULL) && (!input_flag) && (!output_flag)) { // будем бежать по строке, пока не найдём < или >
-        if (!strcmp(list[n], input_symbol) && list[n + 1] != NULL) { // если нашли знак >
-            fd = open(list[n + 1], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR); // открыли файл на запись (считаем, что имя файла идёт сразу после >)
-            input_flag = 1;
-        }
-        if (!strcmp(list[n], output_symbol) && list[n + 1] != NULL) { // если нашли знак <
-            fd = open(list[n + 1], O_RDONLY); // открыли файл на чтение
-            output_flag = 1; // выставили флаг
-        }
-        n++; // обновляем счётчик
-    }
-    if (input_flag) { // если нашли знак >
+void checking_descriptors(char **list, int output_fd, int input_fd, int fd, int n) {
+    char *tmp;
+    if (output_fd == fd) { // если нашли знак >
         dup2(fd, 1); // направили вывод программы в файл
         free(list[n]); // удалили элемент с названием файла
         tmp = list[n - 1]; //
         list[n - 1] = NULL; // конец строки указывает на NULL
         free(tmp); // удалили элемент со знаком >
     }
-    if (output_flag) {
+    if (input_fd == fd) {
         dup2(fd, 0); // теперь считывать будем из файла
         free(list[n]);
         tmp = list[n - 1];
@@ -126,11 +111,31 @@ char **search_io_symbol(char **list, int *input, int *output) {
     if (execvp(list[0], list) < 0) {
         perror("Is failed");
         clear_cmd(list);
-        return NULL;
+        return;
     }
     if (fd > 0) { // если открыли fd, закроем его
         close(fd);
     }
+
+}
+
+char **search_io_symbol(char **list, int *output, int *input) {
+    int n = 0;
+    int fd = -1; // изначально файл не открыт
+    char output_symbol[] = ">", input_symbol[] = "<"; // сохраним символы, которые будем искать
+    int output_fd = *output, input_fd = *input; // для удобства заведём локальные флаги
+    while ((list[n] != NULL) && (output_fd == 1) && (input_fd == 0)) { // будем бежать по строке, пока не найдём < или >
+        if (!strcmp(list[n], output_symbol) && list[n + 1] != NULL) { // если нашли знак >
+            fd = open(list[n + 1], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR); // открыли файл на запись (считаем, что имя файла идёт сразу после >)
+            output_fd = fd;
+        }
+        if (!strcmp(list[n], input_symbol) && list[n + 1] != NULL) { // если нашли знак <
+            fd = open(list[n + 1], O_RDONLY); // открыли файл на чтение
+            input_fd = fd; // выставили флаг
+        }
+        n++; // обновляем счётчик
+    }
+    checking_descriptors(list, output_fd, input_fd, fd, n);
     return list;
 }
 
@@ -180,14 +185,14 @@ char **search_io_symbol(char **list, int *input, int *output) {
 // }
 
 char ***run_commands(char ***list) {
-    int n = 0, input_flag, output_flag;
+    int n = 0, output_fd, input_fd;
     while (list[n] != NULL) {
-        input_flag = 0;
-        output_flag = 0;
+        output_fd = 1;
+        input_fd = 0;
         if (fork() > 0) { // родительский процесс ждёт завершения дочернего
             wait(NULL);
         } else {
-            list[n] = search_io_symbol(list[n], &input_flag, &output_flag); // в дочернем процессе ищем знаки < > в строке
+            list[n] = search_io_symbol(list[n], &output_fd, &input_fd); // в дочернем процессе ищем знаки < > в строке
         }
         n++;
     }
